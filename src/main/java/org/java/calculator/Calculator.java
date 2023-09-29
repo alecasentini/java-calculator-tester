1fc373c8780b76b2c605799356c73f823cd5d3c0
alecasentini <ale.casentini@gmail.com>
2023-09-29 12:36:22 +0200

    Aggunta classe Calculator + metodi

--- src/main/java/org/java/calculator/Calculator.java
+++ src/main/java/org/java/calculator/Calculator.java
@@ -1,5 +1,24 @@
 package org.java.calculator;
 
 public class Calculator {
+	
+    public float add(float num1, float num2) {
+        return num1 + num2;
+    }
 
+    public float subtract(float num1, float num2) {
+        return num1 - num2;
+    }
+
+    public float divide(float num1, float num2) {
+        if (num2 != 0) {
+            return num1 / num2;
+        } else {
+            throw new IllegalArgumentException("Impossibile dividere per 0");
+        }
+    }
+
+    public float multiply(float num1, float num2) {
+        return num1 * num2;
+    }
 }
